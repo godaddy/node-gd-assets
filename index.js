c5d070f1c6921fe74619d3899afe2f1100c04cad
Vincent Fiduccia <vfiduccia@godaddy.com>
2014-04-30 14:32:13 -0700

    Remane groups -> config, support comments in config

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var groups     = require('./lib/groups');
+var config     = require('./lib/config');
 var compiler    = require('./lib/compiler');
 var middleware = require('./lib/middleware');
 var server     = require('./lib/server');
 
-module.exports.groups = groups;
+module.exports.config = config;
 module.exports.compile = compiler;
 module.exports.middleware = middleware;
 module.exports.server = server;
