9691f1c69b4fb54fabc5a543f96ac5f8dd5aaeee
Vincent Fiduccia <vfiduccia@godaddy.com>
2014-05-05 15:56:06 -0700

    Rename top-level exports to capital, move examples

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var config     = require('./lib/config');
-var compiler    = require('./lib/compiler');
-var middleware = require('./lib/middleware');
-var server     = require('./lib/server');
+var Config     = require('./lib/config');
+var Compiler    = require('./lib/compiler');
+var Middleware = require('./lib/middleware');
+var Server     = require('./lib/server');
 
-module.exports.config = config;
-module.exports.compile = compiler;
-module.exports.middleware = middleware;
-module.exports.server = server;
+module.exports.Config = Config;
+module.exports.Compile = Compiler;
+module.exports.Middleware = Middleware;
+module.exports.Server = Server;
